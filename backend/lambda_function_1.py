76bc828ed12ba1d0e9fa5f469c3af48f3a801eb7
Marc Vila <marcvila10@hotmail.com>
2018-10-20 22:08:06 +0200

    Create lambda_function_1.py

---
+++ backend/lambda_function_1.py
@@ -0,0 +1,38 @@
+import boto3
+import json
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    '''HTTP endpoint using API Gateway.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    print("Received event: " + json.dumps(event, indent=2))
+
+    operations = {
+        'DELETE': lambda dynamo, x: dynamo.delete_item(**x),
+        'GET': lambda dynamo, x: dynamo.scan(**x),
+        'POST': lambda dynamo, x: dynamo.put_item(**x),
+        'PUT': lambda dynamo, x: dynamo.update_item(**x),
+    }
+
+    operation = event['httpMethod']
+    if operation in operations:
+        dynamo = boto3.resource('dynamodb').Table('HackUPC2018')
+    
+        payload = event['queryStringParameters'] if operation == 'GET' else event['body']
+        return respond(None, operations[operation](dynamo, payload))
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
