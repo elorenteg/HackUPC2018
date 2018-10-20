beb0bbaba3ede40d3b7ade8571ad771b8d5c422c
elorenteg <ester.lorente@est.fib.upc.edu>
2018-10-20 18:51:01 +0200

    set different dataset points

--- web-app/src/MyHeatmap.js
+++ web-app/src/MyHeatmap.js
@@ -4,19 +4,30 @@ import { Map, TileLayer } from 'react-leaflet';
 import HeatmapLayer from './HeatmapLayer';
 import { addressPoints } from './data/realworld.10000.js';
 
+import { points, points2 } from "./data/data.js";
+
 import "./app.css"
 
 class MyHeatmap extends React.Component {
 
+    // map properties
     state = {
       mapHidden: false,
       layerHidden: false,
-      addressPoints,
-      radius: 4,
-      blur: 8,
-      max: 0.5,
-      limitAddressPoints: true,
+      points: [],
+      radius: 10,
+      blur: 4,
+      min: 0,
+      max: 10
     };
+
+    // gradient of heatmap (value to color matching)
+    gradient = {
+      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
+      0.6: '#FAF3A5', 0.8: '#F5D98B', 1.0: '#DE9A96'
+    };
+
+    
   
     /**
      * Toggle limiting the address points to test behavior with refocusing/zooming when data points change
@@ -28,25 +39,32 @@ class MyHeatmap extends React.Component {
         this.setState({ addressPoints, limitAddressPoints: true });
       }
     }
+
+    setPoints(kpi) {
+      if (kpi == "contenidors") {
+        this.setState({
+          points: points.points,
+          min: points.scale.min,
+          max: points.scale.max/10
+        });
+      }
+      else if (kpi == "verds") {
+        this.setState({
+          points: points2.points,
+          min: points2.scale.min,
+          max: points2.scale.max/10
+        });
+      }
+      else {
+        this.setState({
+          points: [],
+          min: 0,
+          max: 1/10
+        });
+      }
+    }
   
     render() {
-      if (this.state.mapHidden) {
-        return (
-          <div>
-            <input
-              type="button"
-              value="Toggle Map"
-              onClick={() => this.setState({ mapHidden: !this.state.mapHidden })}
-            />
-          </div>
-        );
-      }
-  
-      const gradient = {
-        0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
-        0.6: '#FAF3A5', 0.8: '#F5D98B', '1.0': '#DE9A96'
-      };
-  
       return (
         <div>
           <Map center={[0, 0]} zoom={13}>
@@ -54,11 +72,11 @@ class MyHeatmap extends React.Component {
                 <HeatmapLayer
                   fitBoundsOnLoad
                   fitBoundsOnUpdate
-                  points={this.state.addressPoints}
-                  longitudeExtractor={m => m[1]}
-                  latitudeExtractor={m => m[0]}
-                  gradient={gradient}
-                  intensityExtractor={m => parseFloat(m[2])}
+                  points={this.state.points}
+                  longitudeExtractor={m => m.longitude}
+                  latitudeExtractor={m => m.latitude}
+                  gradient={this.gradient}
+                  intensityExtractor={m => parseFloat(m.value)}
                   radius={Number(this.state.radius)}
                   blur={Number(this.state.blur)}
                   max={Number.parseFloat(this.state.max)}
@@ -69,54 +87,23 @@ class MyHeatmap extends React.Component {
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
           </Map>
-          <input
-            type="button"
-            value="Toggle Map"
-            onClick={() => this.setState({ mapHidden: !this.state.mapHidden })}
-          />
-          <input
-            type="button"
-            value="Toggle Layer"
-            onClick={() => this.setState({ layerHidden: !this.state.layerHidden })}
-          />
-          <input
-            type="button"
-            value="Toggle Limited Data"
-            onClick={this.toggleLimitedAddressPoints.bind(this)}
-          />
-  
+
           <div>
-            Radius
             <input
-              type="range"
-              min={1}
-              max={40}
-              value={this.state.radius}
-              onChange={(e) => this.setState({ radius: e.currentTarget.value })}
-            /> {this.state.radius}
-          </div>
-  
-          <div>
-            Blur
+              type="button"
+              value="Contenidors"
+              onClick={() => this.setPoints("contenidors")}
+            />
             <input
-              type="range"
-              min={1}
-              max={20}
-              value={this.state.blur}
-              onChange={(e) => this.setState({ blur: e.currentTarget.value })}
-            /> {this.state.blur}
-          </div>
-  
-          <div>
-            Max
+              type="button"
+              value="Espais verds"
+              onClick={() => this.setPoints("verds")}
+            />
             <input
-              type="range"
-              min={0.1}
-              max={3}
-              step={0.1}
-              value={this.state.max}
-              onChange={(e) => this.setState({ max: e.currentTarget.value })}
-            /> {this.state.max}
+              type="button"
+              value="Res"
+              onClick={() => this.setPoints()}
+            />
           </div>
         </div>
       );
---
+++ web-app/src/data/data.js
@@ -0,0 +1,67 @@
+export const points = {
+    scale: {
+        min: 0,
+        max: 10
+    },
+    points: [
+        {
+            latitude: 43.2,
+            longitude: 2.1,
+            value: 10
+        },
+        {
+            latitude: 43.2,
+            longitude: 2.1,
+            value: 10
+        },
+        {
+            latitude: 43.2,
+            longitude: 2.1,
+            value: 10
+        },
+        {
+            latitude: 43.2,
+            longitude: 2.1,
+            value: 10
+        },
+        {
+            latitude: 43.4,
+            longitude: 2.13,
+            value: 5
+        }
+    ]
+};
+
+export const points2 = {
+    scale: {
+        min: 0,
+        max: 100
+    },
+    points: [
+        {
+            latitude: 41.2,
+            longitude: 2.1,
+            value: 100
+        },
+        {
+            latitude: 41.2,
+            longitude: 2.1,
+            value: 100
+        },
+        {
+            latitude: 41.2,
+            longitude: 2.1,
+            value: 100
+        },
+        {
+            latitude: 41.2,
+            longitude: 2.1,
+            value: 100
+        },
+        {
+            latitude: 41.4,
+            longitude: 2.13,
+            value: 50
+        }
+    ]
+};
